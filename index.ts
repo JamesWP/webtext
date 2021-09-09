63291e7ff6b052bcf43967bf031a919cdf86febd
James Peach <james_peach01@hotmail.co.uk>
2021-09-09 23:41:16 +0100

    continue rendering

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import * as terminus from './terminus';
+
 window.addEventListener("DOMContentLoaded", () => main());
 
 const text_vertex_shader_source = `#version 300 es
@@ -71,64 +73,121 @@ void main() {
 }
 `;
 
+function textProgram(gl: WebGL2RenderingContext): WebGLProgram {
+  console.debug("Vertex shader source");
+  console.debug(text_vertex_shader_source);
+
+  let text_vertex_shader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(text_vertex_shader, text_vertex_shader_source);
+  gl.compileShader(text_vertex_shader);
+
+  if (!gl.getShaderParameter(text_vertex_shader, gl.COMPILE_STATUS)) {
+    let info = gl.getShaderInfoLog(text_vertex_shader);
+    console.error('Could not compile vert shader WebGL program.', info);
+    return;
+  }
+
+  let text_frag_shader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(text_frag_shader, text_frag_shader_source);
+  gl.compileShader(text_frag_shader);
+
+  if (!gl.getShaderParameter(text_frag_shader, gl.COMPILE_STATUS)) {
+    let info = gl.getShaderInfoLog(text_frag_shader);
+    console.error('Could not compile frag shader WebGL program.', info);
+    return;
+  }
+
+  let text_shader = gl.createProgram();
+
+  gl.attachShader(text_shader, text_vertex_shader);
+  gl.attachShader(text_shader, text_frag_shader);
+
+  gl.linkProgram(text_shader);
+
+  if (!gl.getProgramParameter(text_shader, gl.LINK_STATUS)) {
+    let info = gl.getProgramInfoLog(text_shader);
+    console.error('Link error', info);
+    return;
+  }
+
+  return text_shader;
+}
+
 function resetStyle(el: HTMLElement) {
-    el.style.margin = "0";
-    el.style.padding = "0";
-    el.style.width = "100%";
-    el.style.height = "100%";
+  el.style.margin = "0";
+  el.style.padding = "0";
+  el.style.width = "100%";
+  el.style.height = "100%";
+}
+
+function resizeCanvas(canvas: HTMLCanvasElement) {
+  let multiplier = window.devicePixelRatio;
+  const width = canvas.clientWidth * multiplier | 0;
+  const height = canvas.clientHeight * multiplier | 0;
+  if (canvas.width !== width || canvas.height !== height) {
+    canvas.width = width;
+    canvas.height = height;
+    return true;
+  }
+  return false;
 }
 
 function main() {
-    resetStyle(window.document.body);
-    resetStyle(window.document.documentElement);
+  resetStyle(window.document.body);
+  resetStyle(window.document.documentElement);
 
-    let el = window.document.body;
+  let el = window.document.body;
 
-    let canvas = window.document.createElement("canvas");
-    el.appendChild(canvas);
+  let canvas = window.document.createElement("canvas");
+  el.appendChild(canvas);
 
-    el.style.display = "flex";
-    el.style.alignItems = "stretch";
-    canvas.style.flexGrow = "1";
+  resizeCanvas(canvas);
+  window.addEventListener("resize", () => resizeCanvas(canvas));
 
-    let gl = canvas.getContext("webgl2");
+  el.style.display = "flex";
+  el.style.alignItems = "stretch";
+  canvas.style.flexGrow = "1";
 
-    console.debug("Vertex shader source");
-    console.debug(text_vertex_shader_source);
+  let gl = canvas.getContext("webgl2");
 
-    let text_vertex_shader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(text_vertex_shader, text_vertex_shader_source);
-    gl.compileShader(text_vertex_shader);
+  let text_shader = textProgram(gl);
+  let text_shader_sampler_location = gl.getUniformLocation(text_shader, "font_tex");
+  let text_shader_uniforms_location = gl.getUniformBlockIndex(text_shader, "TextUniforms");
 
-    if (!gl.getShaderParameter(text_vertex_shader, gl.COMPILE_STATUS)) {
-        let info = gl.getShaderInfoLog(text_vertex_shader);
-        console.error('Could not compile vert shader WebGL program.', info);
-        return;
-    }
+  let text_shader_unit = 0;
+  let uniform_binding_point = 0;
 
-    console.debug("Fragment shader source");
-    console.debug(text_frag_shader_source);
+  gl.uniformBlockBinding(text_shader, text_shader_uniforms_location, uniform_binding_point);
+  
+  let font_atlas = terminus.loadTexture(gl);
 
-    let text_frag_shader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(text_frag_shader, text_frag_shader_source);
-    gl.compileShader(text_frag_shader);
+  let uniform_buffer = gl.createBuffer();
+  let uniform_buffer_data = new Float32Array(12);
+  let uniform_buffer_values = {
+    viewport: uniform_buffer_data.subarray(0, 3),
+    origin: uniform_buffer_data.subarray(4, 7),
+    bg_col: uniform_buffer_data.subarray(8, 11),
+  }
 
-    if (!gl.getShaderParameter(text_frag_shader, gl.COMPILE_STATUS)) {
-        let info = gl.getShaderInfoLog(text_frag_shader);
-        console.error('Could not compile frag shader WebGL program.', info);
-        return;
-    }
+  window.requestAnimationFrame(() => {
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
-    let text_shader = gl.createProgram();
+    // bind font atlas
+    gl.useProgram(text_shader);
+    gl.activeTexture(gl.TEXTURE0 + text_shader_unit);
+    gl.bindTexture(gl.TEXTURE_2D, font_atlas);
+    gl.uniform1i(text_shader_sampler_location, text_shader_unit);
 
-    gl.attachShader(text_shader, text_vertex_shader);
-    gl.attachShader(text_shader, text_frag_shader);
+    // set uniforms
+    uniform_buffer_values.viewport.set([-100, -100, 100, 100]);
+    uniform_buffer_values.origin.set([0, 0, 0, 0]);
+    uniform_buffer_values.bg_col.set([0, 0, 0, 0]);
+    gl.bindBuffer(gl.UNIFORM_BUFFER, uniform_buffer);
+    gl.bufferData(gl.UNIFORM_BUFFER, uniform_buffer_data, gl.DYNAMIC_DRAW);
 
-    gl.linkProgram(text_shader);
+    // bind uniforms 
+    gl.bindBufferBase(gl.UNIFORM_BUFFER, uniform_binding_point, uniform_buffer);
 
-    if (!gl.getProgramParameter(text_shader, gl.LINK_STATUS)) {
-        let info = gl.getProgramInfoLog(text_shader);
-        console.error('Link error', info);
-        return;
-    }
+    gl.drawArraysInstanced(gl.TRIANGLE_STRIP, 0, 4, 1);
+  });
 }
